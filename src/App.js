8126ba135a7692ed4bb7570da68e8379baa171da
Maxim-coder-dotcom <pro100max48@gmail.com>
2021-10-04 23:59:45 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Todoolist from "c:/Users/Максим/Desktop/web/react-test/src/Todo/TodoList"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="wrapper">
+  <h1>React Tutorial</h1>
+  <TodoList />
+  </div>
   );
 }
 
